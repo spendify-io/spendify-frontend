55d1fe7cb20dce308e50257fba73e851f012aa46
Ayush Soni <mr.soni1002001@gmail.com>
2022-09-14 17:59:12 +0530

    refactor: formatted files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,17 @@
 import * as React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
-import './index.css';
-import "./styles/index.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import './styles/index.css';
+
+/***** Component Imports *****/
+import Navbar from './components/Navbar';
+import Login from './components/Login';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <BrowserRouter>
+      <Navbar />
+      <Login />
+    </BrowserRouter>
   </ChakraProvider>
 );
